1879b095f205623dce773d4ca0744ee32fb25edd
hates_sundays <sundays@wagn.me>
2017-02-12 15:08:13 +0000

    Prevent pets from attacking friendly targets, e.g. players after duels.

--- src/game/Object/PetAI.cpp
+++ src/game/Object/PetAI.cpp
@@ -76,7 +76,8 @@ void PetAI::MoveInLineOfSight(Unit* u)
 
 void PetAI::AttackStart(Unit* u)
 {
-    if (!u || (m_creature->IsPet() && ((Pet*)m_creature)->getPetType() == MINI_PET))
+    if (!u || (m_creature->IsPet() && ((Pet*)m_creature)->getPetType() == MINI_PET) ||
+        !u->IsTargetableForAttack() || u->IsFriendlyTo(m_creature->GetCharmerOrOwner()))
         { return; }
 
     if (m_creature->Attack(u, true))
